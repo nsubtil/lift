b466f47cbfac2aa817bcf15c148e31333f6e1ae8
Nuno Subtil <nuno.subtil@roche.com>
2016-01-14 12:11:48 -0800

    docs: Switch Sphinx theme, generate Doxygen automatically
    Switches to the readthedocs.org theme. When running the build on
    readthedocs.org, automatically trigger a doxygen build.

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+import subprocess
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -112,7 +113,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -291,3 +292,7 @@ texinfo_documents = [
 
 breathe_projects = { "lift": "doxy" }
 breathe_default_project = "lift"
+
+# trigger doxygen build on readthedocs.org
+if os.environ.get('READTHEDOCS', None):
+    subprocess.call('doxygen config.doxygen', shell=True)
